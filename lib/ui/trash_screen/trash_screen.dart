20a7b67647790b7d4cc5f37923c957a1487c6198
nmheir <nmh7624@gmail.com>
2025-12-04 16:37:01 +0700

    feat: add TrashScreen

--- lib/ui/trash_screen/trash_screen.dart
+++ lib/ui/trash_screen/trash_screen.dart
@@ -1,13 +1,56 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:leafy/data/models/book.dart';
+import 'package:leafy/generated/locale_keys.g.dart';
+import 'package:leafy/main.dart';
+import 'package:leafy/ui/books/widgets/layouts/books_list.dart';
 
 class TrashScreen extends StatelessWidget {
   const TrashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    bookCubit.getDeletedBooks();
+
     return Scaffold(
-      body: Center(
-        child: Text('data'),
+      appBar: AppBar(
+        title: Text(
+          LocaleKeys.deleted_books.tr(),
+          style: const TextStyle(fontSize: 18),
+        ),
+      ),
+      body: StreamBuilder<List<Book>>(
+        stream: bookCubit.deletedBooks,
+        builder: (context, AsyncSnapshot<List<Book>> snapshot) {
+          if (snapshot.hasData) {
+            if (snapshot.data == null || snapshot.data!.isEmpty) {
+              return Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(50),
+                  child: Text(
+                    LocaleKeys.no_deleted_books.tr(),
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      letterSpacing: 1.5,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              );
+            }
+            return BooksList(
+              books: snapshot.data!,
+              listNumber: 5,
+              allBooksCount: null,
+            );
+          } else if (snapshot.hasError) {
+            return Text(
+              snapshot.error.toString(),
+            );
+          } else {
+            return const SizedBox();
+          }
+        },
       ),
     );
   }
